193cd99957c565a4b1ed80dbc515e2fe44df8502
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-07-13 21:24:33 +0900

    add JSON REST-API router

---
+++ server/routes/todo.js
@@ -0,0 +1,62 @@
+const { send, json } = require('micro');
+
+const setApiHeaders = require('./helpers/setApiHeaders');
+const Todo = require('../models/todo');
+
+const list = async (req, res) => {
+  setApiHeaders(res);
+  const conditions = req.params;
+  const todos = await Todo.find(conditions);
+  return todos;
+};
+
+const create = async (req, res) => {
+  setApiHeaders(res);
+  const data = await json(req);
+  const todo = await Todo.create(data);
+  // eslint-disable-next-line no-underscore-dangle
+  res.setHeader('Location', `/todos/${todo._id}`);
+  return send(res, 201, todo);
+};
+
+const show = async (req, res) => {
+  setApiHeaders(res);
+  const todo = await Todo.findById(req.params.id);
+  if (todo === null) {
+    return send(res, 404, {
+      error: { code: 404, message: 'Todo not found' },
+    });
+  }
+  return todo;
+};
+
+const update = async (req, res) => {
+  setApiHeaders(res);
+  const data = await json(req);
+  const todo = await Todo.findByIdAndUpdate(req.params.id, data);
+  if (todo === null) {
+    return send(res, 404, {
+      error: { code: 404, message: 'Todo not found' },
+    });
+  }
+  return send(res, 204, null);
+};
+
+const destory = async (req, res) => {
+  setApiHeaders(res);
+  const todo = await Todo.findByIdAndRemove(req.params.id);
+  if (todo === null) {
+    return send(res, 404, {
+      error: { code: 404, message: 'Todo not found' },
+    });
+  }
+  return send(res, 204, null);
+};
+
+module.exports = {
+  list,
+  create,
+  show,
+  update,
+  destory,
+};
